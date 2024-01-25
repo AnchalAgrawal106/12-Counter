91d9c5659fe9bf33bb9fa595b5edaac3d8e8f157
AnchalAgrawal106 <er.anchalagrawal@gmail.com>
2024-01-25 23:54:03 +0530

    Adding color to Counter Numbers

--- script.js
+++ script.js
@@ -3,20 +3,40 @@ const decrease = document.querySelector('.decrease');
 const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
 
+function numberColor(number1){
+    if(number1 < 0){
+        number.style.color = "red";
+    }
+    else if(number1 > 0){
+        number.style.color = "green";
+    }
+    else {
+        number.style.color = "black";
+    }
+}
+
 decrease.addEventListener('click', () =>{
     let decNo = number.textContent;
     console.log(decNo)
-    decNo = decNo-1;
+    decNo--;
     number.innerHTML = decNo;
+    numberColor(decNo);
 })
 
 reset.addEventListener('click', () =>{
     let resetNo = number.textContent;
+    numberColor(0);
     number.innerHTML = 0;
+    
 })
 
 increase.addEventListener('click', ()=>{
     let increaseNo = number.textContent;
     increaseNo++;
     number.innerHTML = increaseNo;
+    numberColor(increaseNo);
 })
+
+
+
+
